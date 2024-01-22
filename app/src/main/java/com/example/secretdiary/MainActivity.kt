4aef6d4a2cad1a96c7d9723fc3bd1372181b068e
AlenaPanPanda <guiyguker@gmail.com>
2024-01-22 12:25:23 +0100

    Stage 1/5

--- app/src/main/java/com/example/secretdiary/MainActivity.kt
+++ app/src/main/java/com/example/secretdiary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.secretdiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.secretdiary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.saveBtn.setOnClickListener {
+            val note = binding.newWritingEt.text.toString()
+
+            if (note.isNotBlank())
+                postNote(note)
+            else
+                postToast()
+        }
+    }
+
+    private fun postToast() {
+        val textToast = "Empty or blank input cannot be saved"
+        Toast.makeText(this, textToast, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun postNote(note: String) {
+        binding.diaryTv.text = note
+        binding.newWritingEt.setText("")
     }
 }
